cee43f16fdf3be880d30feaaaa77108fe3de98f0
Jean Campelo <jean93campelo@gmail.com>
2022-08-01 16:05:44 -0300

    Estilização tela de sucesso

--- src/Components/Sucess.js
+++ src/Components/Sucess.js
@@ -1,47 +1,73 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export default function Sucess({optionUser}) {
+export default function Sucess({ optionUser }) {
+  let seats = optionUser.seats;
   return (
     <>
-    <Main>
-      <Message>Pedido feito com sucesso!</Message>
-    </Main>
-    <h1>{optionUser.title}</h1>
+      <Message>
+        <h1>Pedido feito com sucesso!</h1>
+      </Message>
 
-    <h1>{optionUser.date}</h1>
+      <Ticket>
+        <h1>Filme e sessão</h1>
+        <h2>{optionUser.title}</h2>
+        <h2>
+          {optionUser.date} - {optionUser.time}
+        </h2>
+      </Ticket>
 
-    <h1>{optionUser.time}</h1>
+      <Ticket>
+        <h1>Ingressos</h1>
+        {seats.map((value) => (
+          <h2> Assento {value}</h2>
+        ))}
+      </Ticket>
 
-    <h1> {optionUser.name} </h1>
-
-    <h1> {optionUser.cpf} </h1>
-
-    <h1> {optionUser.seats} </h1>
-    
-    {console.log(optionUser)}
+      <Ticket>
+        <h1>Comprador</h1>
+        <h2>Nome: {optionUser.name}</h2>
+        <h2>CPF: {optionUser.cpf}</h2>
+      </Ticket>
     </>
   );
 }
 
-const Main = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: pink;
-
-`;
-
-const Message = styled.h1`
-  background-color: var(--color-background-app);
-  height: 100px;
-  width: 180px;
+const Message = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-family: var(--font-primary);
-  font-size: 24px;
-  color: var(--color-message-sucess);
-  margin-top: 66px;
-  font-weight: 700;
-  text-align: center;
+
+  h1 {
+    background-color: var(--color-background-app);
+    height: 100px;
+    width: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: var(--font-primary);
+    font-size: 24px;
+    color: var(--color-message-sucess);
+    margin-top: 66px;
+    font-weight: 700;
+    text-align: center;
+  }
+`;
+
+const Ticket = styled.div`
+  margin: 30px 0 0 30px;
+
+  h1 {
+    font-family: var(--font-primary);
+    font-size: 24px;
+    font-weight: bold;
+    color: var(--color-font);
+    margin: 12px 0;
+  }
+
+  h2 {
+    font-family: var(--font-primary);
+    font-size: 22px;
+    color: var(--color-font);
+    margin: 10px 0;
+  }
 `;
